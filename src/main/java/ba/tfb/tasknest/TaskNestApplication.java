9b6eff0b74a5b042fce593c3ad0c8af07e084bae
JahjaDino <jahjadino@gmail.com>
2026-09-05 23:18:38 +0200

    "Initial setup: Docker, Liquibase, users and roles schema"

---
+++ src/main/java/ba/tfb/tasknest/TaskNestApplication.java
@@ -0,0 +1,13 @@
+package ba.tfb.tasknest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskNestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskNestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ba/tfb/tasknest/TaskNestApplicationTests.java
@@ -0,0 +1,13 @@
+package ba.tfb.tasknest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskNestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
