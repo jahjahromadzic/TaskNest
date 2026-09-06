fbc49e28896bfc79531e1977b2ed088151475464
JahjaDino <jahjadino@gmail.com>
2026-09-06 08:05:03 +0200

    Add indexes, check constraints, equals/hashCode, config profiles and Testcontainers

--- src/main/java/ba/tfb/tasknest/entity/Municipality.java
+++ src/main/java/ba/tfb/tasknest/entity/Municipality.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.Hibernate;
 import org.hibernate.annotations.UuidGenerator;
 
 import java.util.UUID;
@@ -29,4 +30,21 @@ public class Municipality {
 
     @Column(name = "region", length = 100)
     private String region;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) {
+            return false;
+        }
+        Municipality other = (Municipality) o;
+        return id != null && id.equals(other.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return Hibernate.getClass(this).hashCode();
+    }
 }
